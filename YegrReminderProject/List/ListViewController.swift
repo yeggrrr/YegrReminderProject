4d38d59fcf24dffcab8dc364fb79571e6cb497ca
yeggrrr <yjkim981103@gmail.com>
2024-07-02 17:12:41 +0900

    [Delete & Add] ListVC 추가

---
+++ YegrReminderProject/List/ListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ListViewController.swift
+//  YegrReminderProject
+//
+//  Created by YJ on 7/2/24.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+    }
+}
+
