44bd63ce11206d60503ee572bc788dbdaf7e4848
yeggrrr <yjkim981103@gmail.com>
2024-07-02 17:11:35 +0900

    [Add] 패키지 & ListTableViewCell 추가

---
+++ YegrReminderProject/List/ListTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ListTableViewCell.swift
+//  YegrReminderProject
+//
+//  Created by YJ on 7/2/24.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
