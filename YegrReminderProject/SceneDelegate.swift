aaab5dc9da0a338d9891271265198562b94a2eaa
yeggrrr <yjkim981103@gmail.com>
2024-07-04 01:24:22 +0900

    [Fix] SceneDelegate - 초기 VC 변경

--- YegrReminderProject/SceneDelegate.swift
+++ YegrReminderProject/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         
-        let vc = ListViewController()
+        let vc = MainViewController()
         let nav = UINavigationController(rootViewController: vc)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
