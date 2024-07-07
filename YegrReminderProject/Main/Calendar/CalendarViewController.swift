2bf532b5db38927015e4d353ba957cb8d2acc974
yeggrrr <yjkim981103@gmail.com>
2024-07-07 09:54:49 +0900

    [Add] FSCalendar 패키지 추가

---
+++ YegrReminderProject/Main/Calendar/CalendarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarViewController.swift
+//  YegrReminderProject
+//
+//  Created by YJ on 7/7/24.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarViewController: UIViewController {
+    let calendar = FSCalendar()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
