c2ed52c92390d35c27ed56408c58813877a159f4
yeggrrr <yjkim981103@gmail.com>
2024-07-09 22:40:42 +0900

    [Feat] Observable 클래스 생성

---
+++ YegrReminderProject/Observable/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  YegrReminderProject
+//
+//  Created by YJ on 7/9/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        closure(value)
+        self.closure = closure
+    }
+}
